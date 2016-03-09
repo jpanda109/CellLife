911291f1593ef1af97b680eac3ed42b95d39c39a
Jason Xiong <jason.j.xiong@vanderbilt.edu>
2016-03-09 17:47:52 -0600

    conway's game be workin yo

--- main.hs
+++ main.hs
@@ -1,11 +1,16 @@
 import qualified Haste
+import qualified Haste.DOM as DOM
+import qualified Haste.Events as Events
 import qualified Haste.Graphics.Canvas as Canvas
 import qualified Data.Map as Map
-import qualified Data.List as List
+import qualified Data.IORef as IORef
 
 type Grid = Map.Map (Int, Int) Bool
 type Rules = ([Bool], [Bool])
-type Coord = (Int, Int)
+
+defaultRules =
+    --([True | x <- [0..8]], [True | x <- [0..8]])
+    ([x == 2 || x == 3 | x <- [0..8]], [x == 3 | x <- [0..8]])
 
 isAlive :: Grid -> Int -> Int -> Bool
 isAlive grid x y =
@@ -15,8 +20,8 @@ isAlive grid x y =
 
 aliveNeighbors :: Grid -> Int -> Int -> Int
 aliveNeighbors grid x y =
-    let coords = [(x+c1, y+c2) | c1 <- [-1..1], c2 <- [-1..1], c1 /= x && c2 /= y] in
-    foldl (\acc c -> if isAlive grid x y then acc+1 else acc) 0 coords
+    let coords = [(x+c1, y+c2) | c1 <- [-1..1], c2 <- [-1..1], c1 /= 0 || c2 /= 0] in
+    foldl (\acc c -> if uncurry (isAlive grid) c then acc+1 else acc) 0 coords
 
 shouldLive :: Grid -> Rules -> Int -> Int -> Bool
 shouldLive grid rules x y =
@@ -33,14 +38,14 @@ nextState grid rules =
 
 getRect :: Grid -> Int -> Int -> Canvas.Picture ()
 getRect grid x y =
-    Canvas.fill $ Canvas.rect (f $ x*5, f $ y*5) (f $ x*5+5, f $ y*5+5)
+    Canvas.fill $ Canvas.rect (f $ x*20, f $ y*20) (f $ x*20+20, f $ y*20+20)
     where f = fromIntegral
 
 drawCell :: Grid -> Int -> Int -> Canvas.Picture ()
 drawCell grid x y =
     if isAlive grid x y
-      then Canvas.color (Canvas.RGB 255 0 0) (getRect grid x y)
-      else Canvas.color (Canvas.RGB 0 0 0) (getRect grid x y)
+      then Canvas.color (Canvas.RGB 0 0 0) (getRect grid x y)
+      else Canvas.color (Canvas.RGB 255 255 255) (getRect grid x y)
 
 drawGrid :: Canvas.Canvas -> Grid -> IO ()
 drawGrid canvas grid =
@@ -49,11 +54,19 @@ drawGrid canvas grid =
 
 makeGrid :: Int -> Int -> Grid
 makeGrid w h =
-    Map.fromList [((x, y), False) | x <- [0..w], y <- [0..h]]
+    Map.fromList [((x, y), x == 10 || y == 10) | x <- [0..w-1], y <- [0..h-1]]
 
 
 main :: IO()
 main = do
+  grid <- IORef.newIORef $ makeGrid 25 25
   Just canvas <- Canvas.getCanvasById "canvas"
-  drawGrid canvas (makeGrid 10 10)
+  IORef.readIORef grid >>= drawGrid canvas
+  Just nextStateButton <- DOM.elemById "nextStateButton"
+  Events.onEvent nextStateButton Events.Click $ \_ -> do
+    IORef.modifyIORef grid $ \st ->
+      nextState st defaultRules
+    IORef.readIORef grid >>= drawGrid canvas
+    return ()
+  return ()
 
